72a33f74408c75d7e13809d4e2c2efb5c54580fd
Omit-m <omidmolla420@gmail.com>
2026-03-29 12:07:09 +0600

    code

---
+++ First part/Introduce turtle/Turtle1.py
@@ -0,0 +1,12 @@
+import turtle
+import random
+
+for i in range ( 20):
+    x = random.randint(-200, 200)
+    y = random.randint(-200, 200)
+    
+    turtle.penup()
+    turtle.setposition(x, y)
+    turtle.dot()
+
+turtle.done()    
