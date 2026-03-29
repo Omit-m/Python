1a762e4af4bcc249782fa242b1df6d345a35be6b
Omit-m <omidmolla420@gmail.com>
2026-03-29 13:25:47 +0600

    code

---
+++ First part/Introduce turtle/turtle_position.py
@@ -0,0 +1,10 @@
+import turtle
+print(turtle.position() ) # Get the current position of the turtle
+
+
+print(turtle.forward(100)) # Move the turtle forward by 100 units
+print(turtle.position() ) # Get the current position of the turtle after moving forward
+print(turtle.left(90)) # Turn the turtle left by 90 degrees
+turtle.forward(50) # Move the turtle forward by 50 units                                
+print(turtle.position() ) # Get the current position of the turtle after moving forward
+turtle.done()
