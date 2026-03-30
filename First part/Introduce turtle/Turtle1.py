8495707e4f067bb5b99af169db413bf4d105bfb2
Omit-m <omidmolla420@gmail.com>
2026-03-30 11:50:15 +0600

    code

--- First part/Introduce turtle/Turtle1.py
+++ First part/Introduce turtle/Turtle1.py
@@ -1,12 +1,18 @@
 import turtle
 import random
 
-for i in range ( 20):
+turtle.speed(2) # Set the turtle speed to the fastest
+
+colors=    ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+for i in range ( 50):
     x = random.randint(-200, 200)
     y = random.randint(-200, 200)
     
     turtle.penup()
     turtle.setposition(x, y)
+
+    i= random.randint(0, len(colors)-1)
+    turtle.color(colors[i])
     turtle.dot()
 
 turtle.done()    
