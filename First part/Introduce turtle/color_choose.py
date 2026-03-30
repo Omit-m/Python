73fc5bad32771d863f6751d2c97222871a7df444
Omit-m <omidmolla420@gmail.com>
2026-03-30 12:06:06 +0600

    code

---
+++ First part/Introduce turtle/color_choose.py
@@ -0,0 +1,12 @@
+import turtle
+import random
+
+turtle.speed(2)
+
+colors=    ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+i= random.randint(0, len(colors)-1)
+# print(len(colors)) # 6
+
+
+print(i)
+print(colors[i])
