dbf01e7ffacaa5ccc0871337d56d7a43787c683b
Omit-m <omidmolla420@gmail.com>
2026-04-01 15:06:25 +0600

    art by turtle

--- First part/Test programs/turtle.art1.py
+++ First part/Test programs/turtle.art1.py
@@ -1,11 +1,15 @@
 import turtle
+
+turtle.color("red", "green")
 turtle.begin_fill()
 
-turtle.forward(100)
+turtle.forward(200)
 turtle.left(120)
 turtle.forward(200)
 turtle.left(120)
-# turtle.forward(200)
-# turtle.end_fill()
+turtle.forward(200)
+
+
+turtle.end_fill()
 
 turtle.exitonclick()
