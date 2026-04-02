25719d648870fea5dce8048381437a96de6b80db
Omit-m <omidmolla420@gmail.com>
2026-04-02 23:18:32 +0600

    builtins moudle

---
+++ Second Part/Module & Package /Module.py
@@ -0,0 +1,4 @@
+import builtins
+
+print(dir(builtins))
+# This code imports the built-in module 'builtins' and prints a list of all the
---
+++ Second Part/Module & Package /Standard Library/math_module.py
@@ -0,0 +1,16 @@
+# import math
+
+# print(math.pi)
+# # This code imports the math module and prints the value of pi, which is a mathematical constant representing the ratio of a circle's circumference to its diameter. The math module provides various mathematical functions and constants, including pi.
+
+# print(math.pow(2, 3)) 
+# # This code uses the pow function from the math module to calculate 2 raised to the power of 3, which equals 8. The pow function takes two arguments: the base and the exponent, and returns the result of base raised to the power of exponent.
+
+# print(math.sqrt(16))
+# # This code uses the sqrt function from the math module to calculate the square root of 16, which equals 4. The sqrt function takes one argument: a number, and returns its square root.
+
+# print(math.floor(3.7))
+# # This code uses the floor function from the math module to round down the number 3.7 to the nearest whole number, which equals 3. The floor function takes one argument: a number, and returns the largest integer less than or equal to that number.
+
+# print(math.ceil(3.2))
+# # This code uses the ceil function from the math module to round up the number 3.2 to the nearest whole number, which equals 4. The ceil function takes one argument: a number, and returns the smallest integer greater than or equal to that number.
