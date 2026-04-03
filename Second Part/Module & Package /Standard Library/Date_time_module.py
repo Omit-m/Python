f756e2a6a5ba361309f165e85a76990ca0837636
Omit-m <omidmolla420@gmail.com>
2026-04-03 23:17:54 +0600

    datetime  module

---
+++ Second Part/Module & Package /Standard Library/Date_time_module.py
@@ -0,0 +1,8 @@
+import datetime
+
+today = datetime.date.today() # This code imports the 'datetime' module and retrieves today's date using the 'date.today()' method. The result is stored in the variable 'today'.
+print("Today's date:", today)
+
+
+current_time = datetime.datetime.today() # This code retrieves the current date and time using the 'datetime.now()' & 'datetime.today()' method from the 'datetime' module. The result is stored in the variable 'current_time'.
+print("Current date and time:", current_time)
