32197967a5cf933bc6f84cdc6319097d07fd79e5
Omit-m <omidmolla420@gmail.com>
2026-04-04 00:15:59 +0600

    create module

---
+++ Second Part/Module & Package /exam.py
@@ -0,0 +1,16 @@
+def find_fib(n):
+    if n <= 2:
+        return 1 
+    fib_x, fib_next = 1, 1
+    fib_list=[]
+
+    i = 3
+    while i <= n:
+            
+        fib_x, fib_next = fib_next, fib_x + fib_next
+        fib_list.append(fib_next)
+        i += 1  
+    return fib_list
+  
+
+print(find_fib(10))
