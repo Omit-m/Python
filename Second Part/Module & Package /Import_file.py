8b43254b49d38fc72f87f816c57cf174daf0801c
Omit-m <omidmolla420@gmail.com>
2026-04-04 11:30:18 +0600

    import files

--- Second Part/Module & Package /Import_file.py
+++ Second Part/Module & Package /Import_file.py
@@ -3,7 +3,9 @@ import create_module as cm
 
 print( "hello, I am inside exam.py file")
 
+ls =[]
 lm = 16
 for i in range(1, lm):
     n = cm.find_fib(i)
-    print(i, ":", n)
+    ls.append(n)
+print(ls)
