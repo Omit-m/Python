25ff73398f63550f6bfa479ba61794a46777f93b
Omit-m <omidmilla420#gmail.com>
2026-04-05 14:58:16 +0600

    test program

---
+++ Basic/test programs /test1.py
@@ -0,0 +1,24 @@
+
+
+
+
+def multiplication_table(n):
+    for i in range(1, 11):
+        print(n, "*", i, "=", n * i)
+
+def get_input_and_run():
+    n = int(input("Enter a number: "))
+    multiplication_table(n)    
+
+if __name__ == "__main__":
+    get_input_and_run()    
+
+
+
+
+
+        
+
+    
+
+
---
+++ Basic/test programs /test2.py
@@ -0,0 +1,5 @@
+import test1
+
+print ( "I am inside the program test file 2 ")
+
+test1.get_input_and_run()
