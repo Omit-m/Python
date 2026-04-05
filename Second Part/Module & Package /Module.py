3effc89c3fa7583e803210d63f298a61685c848c
Omit-m <omidmolla420@gmail.com>
2026-04-05 15:04:49 +0600

    module works

--- Second Part/Module & Package /Module.py
+++ Second Part/Module & Package /Module.py
@@ -1,4 +1,4 @@
 import datetime
 
 print(dir(datetime))
-# This code imports the built-in module 'builtins' and prints a list of all the
+# This code imports the built-in module 'datetime' and prints a list of all the
