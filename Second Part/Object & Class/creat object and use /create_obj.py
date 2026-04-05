a3d4fef700dca191f31f3ed239db958b3ed93e5b
Omit-m <omidmolla420@gmail.com>
2026-04-06 01:23:38 +0600

    object

---
+++ Second Part/Object & Class/creat object and use /create_obj.py
@@ -0,0 +1,14 @@
+import turtle
+tom = turtle.Turtle()
+
+
+
+nonte = turtle.Turtle()
+fonte = turtle.Turtle()
+
+nonte.shape("circle")
+fonte.shape("square")
+nonte.left(30)
+nonte.forward(100)
+fonte.backward(50)
+turtle.done()
