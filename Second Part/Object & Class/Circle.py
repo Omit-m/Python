6fbfd9e00003cca3a0fc96570435fa096225f5e6
Omit-m <omidmolla420@gmail.com>
2026-04-06 00:56:49 +0600

    object & class

---
+++ Second Part/Object & Class/Circle.py
@@ -0,0 +1,16 @@
+import turtle
+import random
+
+turtle.speed(5)
+turtle.hideturtle() # # makes the turtle invisible
+colors = ["red", "blue", "green", "yellow", "orange", "purple"] 
+
+
+
+for i in range(10):
+    turtle.color(random.choice(colors))
+    turtle.begin_fill()
+    turtle.circle(200)
+    turtle.end_fill()       
+
+turtle.done()
