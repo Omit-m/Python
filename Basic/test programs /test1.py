3ffc7511d10f70cd147d14abfc5743f57b8744ef
Omit-m <omidmolla420@gmail.com>
2026-04-05 15:07:04 +0600

    file imported  code

--- Basic/test programs /test1.py
+++ Basic/test programs /test1.py
@@ -1,7 +1,3 @@
-
-
-
-
 def multiplication_table(n):
     for i in range(1, 11):
         print(n, "*", i, "=", n * i)
--- Basic/test programs /test2.py
+++ Basic/test programs /test2.py
@@ -1,5 +1,3 @@
 import test1
 
-print ( "I am inside the program test file 2 ")
-
 test1.get_input_and_run()
