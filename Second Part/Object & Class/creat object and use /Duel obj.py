db169d596ab1d72fbf93b564f8f8cc47e3c637f9
Omit-m <omidmolla420@gmail.com>
2026-04-07 21:37:30 +0600

    duel object

---
+++ Second Part/Object & Class/creat object and use /Duel obj.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self,n,m,o,p,q):
+        self.name = n
+        self.roll = m
+        self.age = o
+        self.marks = p
+        self.address = q
+
+    def details(self):
+        print("name : ", self.name)
+        print("Roll:", self.roll)
+        print("Age : ",self.age)
+        print("Marks : ", self.marks)
+        print("Address : ", self.address)
+        
+student1= Student("Omit", 550810, 23 , 88 , "Alfadanga")
+student1.details()
+print("\n")
+student2= Student("Tonne", 14, 17 , 81 , "Alfadanga")
+student2.details()
