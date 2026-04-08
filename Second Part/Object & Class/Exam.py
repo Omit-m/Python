73725a29281d78cc5747e1c3b5bf8e139d744a7c
Omit-m <omidmilla420#gmail.com>
2026-04-08 15:46:33 +0600

    exam

---
+++ Second Part/Object & Class/Exam.py
@@ -0,0 +1,7 @@
+class car:
+    def __init__(self,name, manufacturer, color, year, cc):
+        self.name= name
+        self.manufacturer = manufacturer
+        self.color= color
+        self.year= year
+        self.cc=cc
