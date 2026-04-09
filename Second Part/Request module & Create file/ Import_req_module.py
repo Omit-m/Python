4f1fc3d81eeffcb276acd51e976c906c7752b639
Omit-m <omidmilla420#gmail.com>
2026-04-09 23:30:18 +0600

    request info from web

--- Second Part/Request module & Create file/ Import_req_module.py
+++ Second Part/Request module & Create file/ Import_req_module.py
@@ -5,5 +5,5 @@ url = "https://monkeytype.com/"
 response = requests.get(url)
 print(response.ok)
 
-# print(response.status_code)   # shows if request was successful (200 = OK)
-# print(response.text)          # shows the HTML content of the page
+print(response.status_code)   # shows if request was successful (200 = OK)
+print(response.text)          # shows the HTML content of the page
