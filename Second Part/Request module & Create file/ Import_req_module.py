64b31402b597739c16326cfe4d2d2b787229d236
Omit-m <omidmilla420#gmail.com>
2026-04-09 23:49:03 +0600

    repo pblm solve

--- Second Part/Request module & Create file/ Import_req_module.py
+++ Second Part/Request module & Create file/ Import_req_module.py
@@ -6,4 +6,3 @@ response = requests.get(url)
 print(response.ok)
 
 print(response.status_code)   # shows if request was successful (200 = OK)
-print(response.text)          # shows the HTML content of the page
