0a05d880f9969bcb68be6963c16c010113cb89ef
Omit-m <omidmolla420@gmail.com>
2026-04-11 12:13:24 +0600

    file operation by python

--- Second Part/Request module & Create file/File create and write/create_file.py
+++ Second Part/Request module & Create file/File create and write/create_file.py
@@ -1,5 +1,10 @@
-fp= open("file.txt","w")
+# Write mode: overwrites the file
+file1 = open("myfile.txt", "w")
+# L = ["This is Delhi \n", "This is Paris \n", "This is London"]
+file1.write("This is Delhi \nThis is Paris \nThis is London\n")
+file1.close()
 
-fp.write("This is my first file created using python")  
 
-fp.close()
+file1 = open("myfile.txt", "a")
+file1.write("Now I am in Append mode---------------------\nthis is Dhaka\nthis is paris")
+file1.close()
