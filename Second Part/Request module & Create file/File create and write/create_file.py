99d067f5e1e50790b428d92db21714ea819adfc0
Omit-m <omidmolla420@gmail.com>
2026-04-11 12:29:53 +0600

    file operations by python

--- Second Part/Request module & Create file/File create and write/create_file.py
+++ Second Part/Request module & Create file/File create and write/create_file.py
@@ -4,7 +4,24 @@ file1 = open("myfile.txt", "w")
 file1.write("This is Delhi \nThis is Paris \nThis is London\n")
 file1.close()
 
-
+# Append mode: adds to the end of the file
 file1 = open("myfile.txt", "a")
-file1.write("Now I am in Append mode---------------------\nthis is Dhaka\nthis is paris")
+file1.write("this is Dhaka\nthis is paris")
+file1.close()
+
+# Reading file content
+file1 = open("myfile.txt", "r")
+print("Output after appending:\n")
+print(file1.read())
+file1.close()
+
+# Write mode again: overwrites previous content
+file1 = open("myfile.txt", "w")
+file1.write("This is Omit from Dhaka, bangladesh")
+file1.close()
+
+# print the overwriting
+file1 = open("myfile.txt", "r")
+print("Output after overwriting:")
+print(file1.read())
 file1.close()
---
+++ Second Part/Request module & Create file/File create and write/task.py
@@ -0,0 +1,18 @@
+
+"""
+Write a Python program to perform file handling operations as follows:
+1. Create a file named file2.txt and write the line:
+"This is the task file" into it.
+2. Then reopen the same file and write the given line without removing the previous line:
+"This is the appended line of the task file".
+3. Close the file properly after each operation.
+
+"""
+
+file2= open("file2.txt","w")
+file2.write("This is the task file")
+file2.close()
+
+file2= open("file2.txt","a")
+file2.write("\nThis is the appended line of the task file")
+file2.close()
