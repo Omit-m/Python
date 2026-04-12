5b2219f21bd0ef65a92d2e4161d2cc7cfe9233ec
Omit-m <omidmolla420@gmail.com>
2026-04-12 23:19:39 +0600

    task

--- Second Part/Request module & Create file/File create and write/task.py
+++ Second Part/Request module & Create file/File create and write/task.py
@@ -1,6 +1,7 @@
 
 """
 Write a Python program to perform file handling operations as follows:
+
 1. Create a file named file2.txt and write the line:
 "This is the task file" into it.
 2. Then reopen the same file and write the given line without removing the previous line:
