f8fb6d123ac266dd9751cf4fe7cd05570596b3b2
Omit-m <omidmolla420@gmail.com>
2026-04-13 00:07:34 +0600

    file path

--- Basic/os_module.py
+++ Basic/os_module.py
@@ -3,3 +3,5 @@ import os
 file_path = os.path.realpath("os_module.py") # Find the absolute path of the file "os_module.py" in the current directory
 
 print("\n",file_path,"\n") 
+
+print(dir(os)) # List all the attributes and methods of the os module
