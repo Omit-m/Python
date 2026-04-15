b41d2305647a16df59773becbc3a42dfa66371aa
Omit-m <omidmolla420@gmail.com>
2026-04-15 23:34:08 +0600

    for loop inside file

--- Second Part/Lesson_5/Additional file work and expresition/test.py
+++ Second Part/Lesson_5/Additional file work and expresition/test.py
@@ -6,19 +6,19 @@ with open("file.txt", "w") as f:
         f.write(line + "\n")
 
 
-with open("file.txt", "r") as f:
-    content = f.read()
+with open("file.txt", "r") as f: 
+    content = f.read() # read() reads the entire content of the file and returns it as a single string.
     print(content)  
 
 
 with open ("file.txt", "r") as f:
-    lines = f.readlines()
+    lines = f.readlines() # readlines() reads the file and returns a list of lines, where each line is a string in the list.
     print("\n",lines,"\n")
     for line in lines:  
         print(line)  
 
 with open("file.txt", "r") as f:
-    for line in f:
+    for line in f: # When you iterate over a file object, it reads the file line by line, returning each line as a string in each iteration.
         print(line)
 
 
