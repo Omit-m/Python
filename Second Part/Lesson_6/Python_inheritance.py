a6fc8705a834a52ee13068090f9f4cb4b21fe509
Omit-m <omidmolla420@gmail.com>
2026-04-20 13:29:19 +0600

    inheritance

---
+++ Second Part/Lesson_6/Python_inheritance.py
@@ -0,0 +1,36 @@
+class Vehicle:
+    def __init__(self,name, manufacturer, color):
+        self.name= name
+        self.manufacturer= manufacturer
+        self.color= color
+
+    def drive(self):
+        print("Driving", self.manufacturer, self.name)
+
+    def turn(self, direction) :
+        print("Turning", self.name, "to", direction)
+
+    def brake(self):
+        print(self.name, "is stopping!")          
+
+
+
+if __name__ == "__main__":    
+    v1 = Vehicle("Fusion 110 ex", "Waktib", "white")
+    v2 = Vehicle("Softail Delux", "Harley-Davidson", "Blue")
+    v3 = Vehicle("BMW s4", "BMW", "Black")
+
+
+
+    v1.drive()
+    v2.drive()
+    v3.drive()
+
+
+    v1.turn("left")
+    v2.turn("right")
+
+
+    v1.brake()
+    v2.brake()
+    v3.brake()
