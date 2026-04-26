471334484504ff5f595e0eb2eef9fe6282f6c42f
Omit-m <omidmolla420@gmail.com>
2026-04-26 21:36:19 +0600

    python split

--- Basic/Split.py
+++ Basic/Split.py
@@ -1,5 +1,5 @@
-txt = "welcome to the jungle"
-x = txt.split()
+txt = "A story is a connected series of real or imagined events, acting as a narrative, tale, or account designed to entertain, instruct, or inform. Common usages include fairy tales, moral fables, and news reports, while synonyms include narrative, account, anecdote, yarn, tale, and Chronicle"
+x = txt.split("s")
 
 print(x)
 
