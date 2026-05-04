631a8ac10e24125246c9f85fc57629689339ad2e
Omit-m <omidmolla420@gmail.com>
2026-05-04 23:21:32 +0600

    declaration

--- Second Part/Lesson_7/Re_findnumbers.py
+++ Second Part/Lesson_7/Re_findnumbers.py
@@ -3,8 +3,25 @@ import re
 
 text = "Age 22 and 105"
 
+# findall() → gets all numbers
 single_digits = re.findall(r'\d', text)
 multiple_digits = re.findall(r'\d+', text)
 
 print(single_digits)
 print(multiple_digits)
+
+
+# Search for the first occurrence of one or more digits in the text.
+# search() → finds first number
+
+import re
+
+text = "I have 2 apples and 5 bananas"
+
+m = re.search(r'\d+', text)
+print(m.group())
+
+
+
+
+
--- Second Part/Lesson_7/Re_replace.py
+++ Second Part/Lesson_7/Re_replace.py
@@ -3,6 +3,6 @@ import re
 text = "My email is omit123@gmail.com and I love Bangladesh. Call me at 01872035199."
 
 
-# sub() → replace text
+# sub() → replaces "Bangladesh" → "Canada"
 new_text = re.sub(r'Bangladesh', 'Canada', text)
 print("sub():", new_text)
