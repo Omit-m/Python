165296323aa38e65e3eefd04f4ed7a07cf950f6c
Omit-m <omidmolla420@gmail.com>
2026-05-04 23:06:55 +0600

    Python split

--- Basic/Split.py
+++ Basic/Split.py
@@ -1,5 +1,7 @@
 txt = "A story is a connected series of real or imagined events, acting as a narrative, tale, or account designed to entertain, instruct, or inform. Common usages include fairy tales, moral fables, and news reports, while synonyms include narrative, account, anecdote, yarn, tale, and Chronicle"
-x = txt.split("s")
+
+lower_txt = txt.lower()
+x = lower_txt.split(",")
 
 print(x)
 
