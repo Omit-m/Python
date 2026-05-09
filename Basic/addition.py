e83446f46433a58fe1c4f46865a47ef79fc179fd
Omit-m <omidmolla420@gmail.com>
2026-05-09 21:34:16 +0600

    Basic

--- Basic/addition.py
+++ Basic/addition.py
@@ -1,7 +1,14 @@
-A = int ( input( " Enter a number : "))
-B = int ( input( " Enter a number : "))
+A = int ( input( " Enter a even number : "))
+B = int ( input( " Enter a odd number : "))
 
-sum= A + B 
 
-print(" sum = ", sum )
+if A % 2 == 0 & B % 2 ==1 :
+ sum= A + B 
+ print(" sum = ", sum )
+
+else :
+    print(" Invalid input ") 
+
+
+
 
