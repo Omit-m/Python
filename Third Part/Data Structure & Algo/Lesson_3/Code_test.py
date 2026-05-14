285c469e34aa1ae1bfe15aa740e5ddeca4fb5a27
Omit-m <omidmolla420@gmail.com>
2026-05-15 01:11:25 +0600

    code testing

--- Third Part/Data Structure & Algo/Lesson_3/Code_test.py
+++ Third Part/Data Structure & Algo/Lesson_3/Code_test.py
@@ -1,8 +1,21 @@
-def average(l):
-    if not l:
-        return None
-    return sum(l) / len(l)
+# def average(l):
+#     if not l:
+#         return None
+#     return sum(l) / len(l)
 
-if __name__ == "__main__":  
-    l = [1, 2, 3, 4, 5]
-    print(average(l))
+# if __name__ == "__main__":  
+#     l = [1, 2, 3, 4, 5,6]
+#     print(average(l))
+
+
+# ....................
+
+if __name__ == "__main__":
+    L = [1,2,3,4,5]
+    expected_result = 3
+    avg_result = sum(L) / len(L)
+
+    if expected_result == avg_result:
+        print("Test passed")
+    else:
+        print("Test failed!", "received:", avg_result, "expected:", expected_result) 
