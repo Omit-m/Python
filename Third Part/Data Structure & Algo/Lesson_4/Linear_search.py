efeff75c06f5474b339993cc1de601aa9d9afb7c
Omit-m <omidmolla420@gmail.com>
2026-06-24 10:05:34 +0600

    lenear search

---
+++ Third Part/Data Structure & Algo/Lesson_4/Linear_search.py
@@ -0,0 +1,15 @@
+def linear_search(list, target):
+
+ list_length = len(list)
+ i = 0
+
+ while i < list_length:
+  if list[i] == target:
+   return i
+  i += 1
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 5
+result = linear_search(list, target) 
+
+print(f'Target digit {target} found at index: {result}')
